07e46600f27da61856bcf81df18ba119b433a787
MateusCampaner <82098812+MateusCampaner@users.noreply.github.com>
2023-06-15 18:27:44 -0300

    v3
    v3

--- Gauss.py
+++ Gauss.py
@@ -11,30 +11,94 @@ metodo = st.sidebar.selectbox(
     ('Gauss', 'Gauss - Seidel'))
 
 def gerar_matriz(size):
-        matrix = np.zeros((size, size+1))
+        matriz = np.zeros((size, size+1))
         for i in range(size):
             for j in range(size+1):
-                matrix[i][j] = np.random.randint(10)  # Você pode ajustar o intervalo dos valores aqui
+                matriz[i][j] = np.random.randint(10)  # Você pode ajustar o intervalo dos valores aqui
 
-        return matrix
+        return matriz
     
-def main():
-    size = st.sidebar.number_input("Digite o tamanho da matriz:", min_value=1, value=3, step=1)
-
-    if st.sidebar.button("Gerar matriz"):
-        matrix = gerar_matriz(int(size))
-        st.write("Matriz gerada:")
-        st.write(matrix)
-
-if __name__ == "__main__":
-    main()
 
 
 if metodo == 'Gauss':
     st.write('Você selecionou Gauss.')
 
+    def resolver_gauss(matrix):
+        size = matrix.shape[0]
+
+        for i in range(size):
+            if matrix[i][i] == 0:
+                raise ValueError("A matriz não pode ter zeros na diagonal principal.")
+
+            for j in range(i+1, size):
+                ratio = matrix[j][i] / matrix[i][i]
+                for k in range(size+1):
+                    matrix[j][k] = matrix[j][k] - ratio * matrix[i][k]
+
+        x = np.zeros(size)
+        for i in range(size-1, -1, -1):
+            x[i] = matrix[i][size] / matrix[i][i]
+            for j in range(i-1, -1, -1):
+                matrix[j][size] -= matrix[j][i] * x[i]
+
+        return x
+    
+    def main():
+        size = st.sidebar.number_input("Digite o tamanho da matriz:", min_value=1, value=3, step=1)
+
+        if st.sidebar.button("Gerar matriz"):
+            matrix = gerar_matriz(int(size))
+            st.write("Matriz gerada:")
+            st.write(matrix)
+
+            try:
+                solution = resolver_gauss(matrix)
+                st.write("Solução encontrada:")
+                st.write(solution)
+            except ValueError as e:
+                st.write("Erro:", str(e))
+
+    if __name__ == "__main__":
+        main()
+
 else:
     st.write("Você selecionou Gauss - Seidel")
 
+    def resolver_gauss_seidel(matrix):
+        size = matrix.shape[0]
 
+        for i in range(size):
+            if matrix[i][i] == 0:
+                raise ValueError("A matriz não pode ter zeros na diagonal principal.")
+
+            for j in range(i+1, size):
+                ratio = matrix[j][i] / matrix[i][i]
+                for k in range(size+1):
+                    matrix[j][k] = matrix[j][k] - ratio * matrix[i][k]
+
+        x = np.zeros(size)
+        for i in range(size-1, -1, -1):
+            x[i] = matrix[i][size] / matrix[i][i]
+            for j in range(i-1, -1, -1):
+                matrix[j][size] -= matrix[j][i] * x[i]
+
+        return x
+    
+    def main():
+        size = st.sidebar.number_input("Digite o tamanho da matriz:", min_value=1, value=3, step=1)
+
+        if st.sidebar.button("Gerar matriz"):
+            matrix = gerar_matriz(int(size))
+            st.write("Matriz gerada:")
+            st.write(matrix)
+
+            try:
+                solution = resolver_gauss_seidel(matrix)
+                st.write("Solução encontrada:")
+                st.write(solution)
+            except ValueError as e:
+                st.write("Erro:", str(e))
+
+    if __name__ == "__main__":
+        main()
 
--- teste.py
+++ teste.py
@@ -1,27 +1,50 @@
-import streamlit as st
 import numpy as np
+import streamlit as st
 
-def generate_matrix(size):
-    # Criar a matriz
+def gerar_matriz(size):
     matrix = np.zeros((size, size+1))
-
-    # Preencher a matriz com valores aleatórios
     for i in range(size):
         for j in range(size+1):
             matrix[i][j] = np.random.randint(10)  # Você pode ajustar o intervalo dos valores aqui
 
     return matrix
 
-# Interface do Streamlit
+def resolver_matriz(matrix):
+    size = matrix.shape[0]
+
+    # Eliminação de Gauss
+    for i in range(size):
+        if matrix[i][i] == 0:
+            raise ValueError("A matriz não pode ter zeros na diagonal principal.")
+
+        for j in range(i+1, size):
+            ratio = matrix[j][i] / matrix[i][i]
+            for k in range(size+1):
+                matrix[j][k] = matrix[j][k] - ratio * matrix[i][k]
+
+    # Retrosubstituição
+    x = np.zeros(size)
+    for i in range(size-1, -1, -1):
+        x[i] = matrix[i][size] / matrix[i][i]
+        for j in range(i-1, -1, -1):
+            matrix[j][size] -= matrix[j][i] * x[i]
+
+    return x
+
 def main():
-    st.title("Gerador de Matriz")
+    size = st.sidebar.number_input("Digite o tamanho da matriz:", min_value=1, value=3, step=1)
 
-    size = st.number_input("Digite o tamanho da matriz:", min_value=1, value=3, step=1)
-
-    if st.button("Gerar matriz"):
-        matrix = generate_matrix(int(size))
+    if st.sidebar.button("Gerar matriz"):
+        matrix = gerar_matriz(int(size))
         st.write("Matriz gerada:")
         st.write(matrix)
+
+        try:
+            solution = resolver_matriz(matrix)
+            st.write("Solução encontrada:")
+            st.write(solution)
+        except ValueError as e:
+            st.write("Erro:", str(e))
 
 if __name__ == "__main__":
     main()
